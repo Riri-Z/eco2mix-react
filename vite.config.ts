7d624a5994d6594ff4869126079f5a712e6d3722
Riri-Z <riri.zamor@gmail.com>
2024-06-11 16:56:04 +0200

    Dockerized dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    watch:{
+      usePolling:true
+    },
+    host:true,
+    strictPort:true,
+    port: 5173
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
